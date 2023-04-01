e9a2db2cb9d8b1b6001d172571764b048accace0
yury-yury <yury_yury@mail.ru>
2023-04-01 11:17:33 +0300

    tesk1 is ready

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def my_object():
+    return Item(name="test", price=100, quantity=10)
+
+
+def test_start_init(my_object):
+    assert my_object.name == "test"
+    assert my_object.price == 100
+    assert my_object.quantity == 10
+
+
+def test_calculate_total_price(my_object):
+    assert my_object.calculate_total_price() == 1000
+
+
+def test_apply_discount(my_object):
+    Item.pay_rate = 0.8
+    my_object.apply_discount()
+    assert my_object.price == 80
